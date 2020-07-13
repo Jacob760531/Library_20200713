ce3fc894b9d488862419ca81786eb49bb10b607d
조장우 <jopuk2@gmail.com>
2020-07-13 20:16:18 +0900

    사진 크게보기 화면 진입 => 확대 축소 지원안됨

---
+++ app/src/main/java/kr/co/tjoeun/library_20200713/ProfilePhotoActivity.kt
@@ -0,0 +1,30 @@
+package kr.co.tjoeun.library_20200713
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ProfilePhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_photo)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+        goToPhotoViewBtn.setOnClickListener {
+            val myIntent = Intent(mContext, ProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+    }
+
+    override fun setValues() {
+    }
+
+}
